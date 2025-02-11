12673f7c14e67b1c9a51acebece9ae83a6598884
kakuza73 <stas-e93@inbox.ru>
2025-02-11 12:04:50 +0600

    Update

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,28 +9,30 @@ const __dirname = path.dirname(__filename);
 const fixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(fixturePath(filename), 'utf-8');
 
-const filePath1 = fixturePath('file1.json');
-const filePath2 = fixturePath('file2.yaml');
 const expectJson = readFile(fixturePath('expectJson.txt'));
 const expectStylish = readFile(fixturePath('expectStylish.txt'));
 const expectPlain = readFile(fixturePath('expectPlain.txt'));
 
 describe('gendiff', () => {
   test('unsupport format', () => {
-    const data = 'data';
-    const format = 'txt';
-    expect(() => parse(format, data)).toThrow('Not support format: txt');
+    expect(() => parse('txt', 'data')).toThrow('Not support format: txt');
   });
 
-  test('Stylish builder', () => {
+  const formats = ['json', 'yml', 'yaml'];
+  const outputs = [
+    ['stylish', expectStylish],
+    ['plain', expectPlain],
+    ['json', expectJson],
+  ];
+
+  test.each(formats)('compare %s files', (format) => {
+    const filePath1 = fixturePath(`file1.${format}`);
+    const filePath2 = fixturePath(`file2.${format}`);
+
+    outputs.forEach(([outputFormat, expected]) => {
+      expect(gendiff(filePath1, filePath2, outputFormat)).toEqual(expected);
+    });
+
     expect(gendiff(filePath1, filePath2)).toEqual(expectStylish);
-  });
-
-  test('Plain builder', () => {
-    expect(gendiff(filePath1, filePath2, 'plain')).toEqual(expectPlain);
-  });
-
-  test('Json builder', () => {
-    expect(gendiff(filePath1, filePath2, 'json')).toEqual(expectJson);
   });
 });
